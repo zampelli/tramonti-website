ebe34ad3749cc68e65f8e69ef3426ef860d62d17
zampelli <milano121293@gmail.com>
2019-11-05 14:22:54 -0800

    database-features

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,14 @@
+
 // Get modal elements
 const modal = document.getElementById('modalSpecial');
 const modalBtn = document.querySelector('.modalBtn');
 const specialLink = document.querySelector('.specialLink');
 const closeBtn = document.querySelector('.close-btn');
-const editBtn = document.querySelector('.edit-btn');
-const hideSubmit = document.querySelector('.hide');
-let edit = document.querySelector('.text-content');
-let input;
-
-// As default the content cannot be editable
-edit.setAttribute('contenteditable', false);
-// Submit button it's only visible if the contenteditable is True
-hideSubmit.style.display = 'none';
+const closeBtnLogIn = document.querySelector('.close-log-in');
+const modalLog = document.querySelector('.modal-log');
+const editBtn = document.querySelector('.edit');
+const hide = document.querySelector('.hide');
+const show = document.querySelector('.show');
 
 function openModal() {
   modal.style.display = 'block';
@@ -19,25 +16,25 @@ function openModal() {
 
 function closeModal() {
   modal.style.display = 'none';
-  hideSubmit.style.display = 'none';
-  edit.setAttribute("contenteditable", false);
+  hide.classList.toggle('close');
+  setTimeout(function (){
+    hide.classList.remove('close');
+    hide.classList.remove('show');
+  },1000);
+  
+  // hideSubmit.style.display = 'none';
+  // edit.setAttribute("contenteditable", false);
 };
 
-function editContent() {
- input = prompt('Are you the Admin?');
- if(input === '1234') {
-  edit.setAttribute("contenteditable", true);
-  hideSubmit.style.display = 'block';
- } else {
-  edit.setAttribute("contenteditable", false);
- }
+function editContent(){
+  // hide.classList.toggle('hide');
+  hide.classList.toggle('show');
+  
 };
 
 function clickOutside(e) {
   if(e.target === modal){
     modal.style.display = 'none';
-    hideSubmit.style.display = 'none';
-    edit.setAttribute("contenteditable", false);
   }
 };
 
@@ -45,6 +42,7 @@ function clickOutside(e) {
 modalBtn.addEventListener('click', openModal);
 specialLink.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
+closeBtnLogIn.addEventListener('click', closeModal);
 editBtn.addEventListener('click', editContent);
 window.addEventListener('click', clickOutside);
 
