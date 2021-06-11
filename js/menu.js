647a11736a411b8e8ec6e29f22401a87abc243cd
zampelli <info@zampelli.design>
2021-06-11 12:10:20 -0700

    buttons to show pdf menu created, now we test on mobile

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const dinnerBtn = document.querySelector(".dinner-btn");
+const dinnerPDF = document.querySelector(".dinner-pdf");
+
+dinnerBtn.addEventListener("click", () => {
+dinnerPDF.style.display = "block";
+})
