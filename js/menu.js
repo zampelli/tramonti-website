2faf5ed7a1293e389103d46b9c2a5c0cd9b17f72
zampelli <info@zampelli.design>
2021-06-11 12:27:27 -0700

    testing opening closing pdf modal on small screen and different broswers

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,11 @@
 const dinnerBtn = document.querySelector(".dinner-btn");
 const dinnerPDF = document.querySelector(".dinner-pdf");
+const closePDF = document.querySelector(".close-btn");
 
 dinnerBtn.addEventListener("click", () => {
-dinnerPDF.style.display = "block";
-})
+  dinnerPDF.style.display = "block";
+});
+
+closePDF.addEventListener("click", () => {
+  dinnerPDF.style.display = "none";
+});
