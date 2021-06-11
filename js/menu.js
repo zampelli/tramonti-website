a6a3efb4dca00a08949f64ca9c4fef8d91c76260
zampelli <info@zampelli.design>
2021-06-11 12:59:27 -0700

    testing with netlify

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,8 @@ const dinnerBtn = document.querySelector(".dinner-btn");
 const dinnerPDF = document.querySelector(".dinner-pdf");
 const drinkBtn = document.querySelector(".drink-btn");
 const drinkPDF = document.querySelector(".drink-pdf");
-const closePDF = document.querySelector(".close-btn");
+const closeDinnerPDF = document.querySelector(".close-btn");
+const closeDrinkPDF = document.querySelector(".close-btn2");
 
 
 dinnerBtn.addEventListener("click", () => {
@@ -13,10 +14,13 @@ drinkBtn.addEventListener("click", () => {
     drinkPDF.style.display = "block";
   });
 
-closePDF.addEventListener("click", () => {
+closeDinnerPDF.addEventListener("click", () => {
   dinnerPDF.style.display = "none";
-  console.log("click")
 });
+
+closeDrinkPDF.addEventListener("click", () => {
+    drinkPDF.style.display = "none";
+})
 
 
 
