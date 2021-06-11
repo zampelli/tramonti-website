5bdb6676b41ed7c24342798e95281f6e213ff093
zampelli <info@zampelli.design>
2021-06-11 13:09:04 -0700

    final test for new pdf menu style

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,11 @@ const dinnerBtn = document.querySelector(".dinner-btn");
 const dinnerPDF = document.querySelector(".dinner-pdf");
 const drinkBtn = document.querySelector(".drink-btn");
 const drinkPDF = document.querySelector(".drink-pdf");
+const dessertPDF = document.querySelector(".dessert-pdf");
+const dessertBtn = document.querySelector(".dessert-btn");
 const closeDinnerPDF = document.querySelector(".close-btn");
 const closeDrinkPDF = document.querySelector(".close-btn2");
+const closeDessertPDF = document.querySelector(".close-btn3");
 
 
 dinnerBtn.addEventListener("click", () => {
@@ -12,7 +15,11 @@ dinnerBtn.addEventListener("click", () => {
 
 drinkBtn.addEventListener("click", () => {
     drinkPDF.style.display = "block";
-  });
+});
+
+dessertBtn.addEventListener("click", () => {
+    dessertPDF.style.display = "block";
+});
 
 closeDinnerPDF.addEventListener("click", () => {
   dinnerPDF.style.display = "none";
@@ -20,6 +27,10 @@ closeDinnerPDF.addEventListener("click", () => {
 
 closeDrinkPDF.addEventListener("click", () => {
     drinkPDF.style.display = "none";
+})
+
+closeDessertPDF.addEventListener("click", () => {
+    dessertPDF.style.display = "none";
 })
 
 
