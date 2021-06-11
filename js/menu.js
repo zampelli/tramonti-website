d2914cff666cdf5a5516942efc11c90ecf0450f5
zampelli <info@zampelli.design>
2021-06-11 12:53:30 -0700

    checking wierd closeBtn bug on mobile

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,22 @@
 const dinnerBtn = document.querySelector(".dinner-btn");
 const dinnerPDF = document.querySelector(".dinner-pdf");
+const drinkBtn = document.querySelector(".drink-btn");
+const drinkPDF = document.querySelector(".drink-pdf");
 const closePDF = document.querySelector(".close-btn");
+
 
 dinnerBtn.addEventListener("click", () => {
   dinnerPDF.style.display = "block";
 });
 
+drinkBtn.addEventListener("click", () => {
+    drinkPDF.style.display = "block";
+  });
+
 closePDF.addEventListener("click", () => {
   dinnerPDF.style.display = "none";
+  console.log("click")
 });
+
+
+
